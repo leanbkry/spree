6b54269166eda42cbfaedb7f92abc75cc301dfb2
Florian Bergmann <florian.bergmann@leanbakery.com>
2020-04-30 22:07:59 +0200

    add flag to disable api v1 routes
    If you start a new spree project and you do not have legacy clients you can silence those routes and disable potential unwanted requests

--- api/app/models/spree/api_configuration.rb
+++ api/app/models/spree/api_configuration.rb
@@ -1,6 +1,7 @@
 module Spree
   class ApiConfiguration < Preferences::Configuration
     preference :requires_authentication, :boolean, default: true
+    preference :disable_v1_routes, :boolean, default: false
     preference :api_v2_content_type, :string, default: 'application/vnd.api+json'
   end
 end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -6,124 +6,126 @@ end
 
 Spree::Core::Engine.add_routes do
   namespace :api, defaults: { format: 'json' } do
-    namespace :v1 do
-      resources :promotions, only: [:show]
+    unless Spree::Api::Config[:disable_v1_routes]
+      namespace :v1 do
+        resources :promotions, only: [:show]
 
-      resources :customer_returns, only: [:index]
-      resources :reimbursements, only: [:index]
+        resources :customer_returns, only: [:index]
+        resources :reimbursements, only: [:index]
 
-      resources :products do
-        resources :images
-        resources :variants
-        resources :product_properties
-      end
-
-      concern :order_routes do
-        member do
-          put :approve
-          put :cancel
-          put :empty
-          put :apply_coupon_code
+        resources :products do
+          resources :images
+          resources :variants
+          resources :product_properties
         end
 
-        resources :line_items
-        resources :payments do
+        concern :order_routes do
           member do
-            put :authorize
-            put :capture
-            put :purchase
-            put :void
-            put :credit
-          end
-        end
-
-        resources :addresses, only: [:show, :update]
-
-        resources :return_authorizations do
-          member do
-            put :add
+            put :approve
             put :cancel
-            put :receive
+            put :empty
+            put :apply_coupon_code
+          end
+
+          resources :line_items
+          resources :payments do
+            member do
+              put :authorize
+              put :capture
+              put :purchase
+              put :void
+              put :credit
+            end
+          end
+
+          resources :addresses, only: [:show, :update]
+
+          resources :return_authorizations do
+            member do
+              put :add
+              put :cancel
+              put :receive
+            end
           end
         end
-      end
 
-      resources :checkouts, only: [:update], concerns: :order_routes do
-        member do
-          put :next
-          put :advance
+        resources :checkouts, only: [:update], concerns: :order_routes do
+          member do
+            put :next
+            put :advance
+          end
         end
-      end
 
-      resources :variants do
-        resources :images
-      end
+        resources :variants do
+          resources :images
+        end
 
-      resources :option_types do
+        resources :option_types do
+          resources :option_values
+        end
         resources :option_values
-      end
-      resources :option_values
 
-      resources :option_values, only: :index
+        resources :option_values, only: :index
 
-      get '/orders/mine', to: 'orders#mine', as: 'my_orders'
-      get '/orders/current', to: 'orders#current', as: 'current_order'
+        get '/orders/mine', to: 'orders#mine', as: 'my_orders'
+        get '/orders/current', to: 'orders#current', as: 'current_order'
 
-      resources :orders, concerns: :order_routes do
-        put :remove_coupon_code, on: :member
-      end
+        resources :orders, concerns: :order_routes do
+          put :remove_coupon_code, on: :member
+        end
 
-      resources :zones
-      resources :countries, only: [:index, :show] do
+        resources :zones
+        resources :countries, only: [:index, :show] do
+          resources :states, only: [:index, :show]
+        end
+
+        resources :shipments, only: [:create, :update] do
+          collection do
+            post 'transfer_to_location'
+            post 'transfer_to_shipment'
+            get :mine
+          end
+
+          member do
+            put :ready
+            put :ship
+            put :add
+            put :remove
+          end
+        end
         resources :states, only: [:index, :show]
-      end
 
-      resources :shipments, only: [:create, :update] do
-        collection do
-          post 'transfer_to_location'
-          post 'transfer_to_shipment'
-          get :mine
-        end
-
-        member do
-          put :ready
-          put :ship
-          put :add
-          put :remove
-        end
-      end
-      resources :states, only: [:index, :show]
-
-      resources :taxonomies do
-        member do
-          get :jstree
-        end
-        resources :taxons do
+        resources :taxonomies do
           member do
             get :jstree
           end
+          resources :taxons do
+            member do
+              get :jstree
+            end
+          end
         end
+
+        resources :taxons, only: [:index]
+
+        resources :inventory_units, only: [:show, :update]
+
+        resources :users do
+          resources :credit_cards, only: [:index]
+        end
+
+        resources :properties
+        resources :stock_locations do
+          resources :stock_movements
+          resources :stock_items
+        end
+
+        resources :stock_items, only: [:index, :update, :destroy]
+        resources :stores
+
+        put '/classifications', to: 'classifications#update', as: :classifications
+        get '/taxons/products', to: 'taxons#products', as: :taxon_products
       end
-
-      resources :taxons, only: [:index]
-
-      resources :inventory_units, only: [:show, :update]
-
-      resources :users do
-        resources :credit_cards, only: [:index]
-      end
-
-      resources :properties
-      resources :stock_locations do
-        resources :stock_movements
-        resources :stock_items
-      end
-
-      resources :stock_items, only: [:index, :update, :destroy]
-      resources :stores
-
-      put '/classifications', to: 'classifications#update', as: :classifications
-      get '/taxons/products', to: 'taxons#products', as: :taxon_products
     end
 
     namespace :v2 do
@@ -166,26 +168,28 @@ Spree::Core::Engine.add_routes do
 
     get '/404', to: 'errors#render_404'
 
-    match 'v:api/*path', to: redirect { |params, request|
-      format = ".#{params[:format]}" unless params[:format].blank?
-      query  = "?#{request.query_string}" unless request.query_string.blank?
-
-      if request.path == "#{spree_path}api/v1/#{params[:path]}#{format}#{query}"
-        "#{spree_path}api/404"
-      else
-        "#{spree_path}api/v1/#{params[:path]}#{format}#{query}"
-      end
-    }, via: [:get, :post, :put, :patch, :delete]
-
-    match '*path', to: redirect { |params, request|
-      format = ".#{params[:format]}" unless params[:format].blank?
-      query  = "?#{request.query_string}" unless request.query_string.blank?
-
-      if request.path == "#{spree_path}api/v1/#{params[:path]}#{format}#{query}"
-        "#{spree_path}api/404"
-      else
-        "#{spree_path}api/v1/#{params[:path]}#{format}#{query}"
-      end
-    }, via: [:get, :post, :put, :patch, :delete]
+    unless Spree::Api::Config[:disable_v1_routes]
+      match 'v:api/*path', to: redirect { |params, request|
+        format = ".#{params[:format]}" unless params[:format].blank?
+        query  = "?#{request.query_string}" unless request.query_string.blank?
+    
+        if request.path == "#{spree_path}api/v1/#{params[:path]}#{format}#{query}"
+          "#{spree_path}api/404"
+        else
+          "#{spree_path}api/v1/#{params[:path]}#{format}#{query}"
+        end
+      }, via: [:get, :post, :put, :patch, :delete]
+    
+      match '*path', to: redirect { |params, request|
+        format = ".#{params[:format]}" unless params[:format].blank?
+        query  = "?#{request.query_string}" unless request.query_string.blank?
+    
+        if request.path == "#{spree_path}api/v1/#{params[:path]}#{format}#{query}"
+          "#{spree_path}api/404"
+        else
+          "#{spree_path}api/v1/#{params[:path]}#{format}#{query}"
+        end
+      }, via: [:get, :post, :put, :patch, :delete]
+    end
   end
 end
